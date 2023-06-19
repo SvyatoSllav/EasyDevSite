fd880a06963309279649d3452a5c37512ab6aab3
Александр <tarasov.slavas2002@gmail.com>
2023-06-20 02:20:42 +0300

    бакфик енвов

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
-const Dotenv = require("dotenv-webpack");
+// const Dotenv = require("dotenv-webpack");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-module.exports = {
-  configureWebpack: {
-    plugins: [new Dotenv()],
-  },
-};
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [new Dotenv()],
+//   },
+// };
